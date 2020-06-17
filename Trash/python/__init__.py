7e64a8fe52b13c67d1d0c30b02aa1d3339243286
David Ávila Quezada <david.avila@uach.cl>
2020-06-17 10:24:20 -0400

    nueva versión v20.6

--- Trash/python/__init__.py
+++
@@ -1,5 +0,0 @@
-from pyforms 			import BaseWidget
-from pyforms.Controls 	import *
-import cv2
-
-import pyforms
--- Trash/python/triada.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# main.py
-
-import menu
-
-mi_menu = menu.Menu()
-mi_menu.loop()
