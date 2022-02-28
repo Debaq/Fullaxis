2c90e699bdfc7049035eff751d6583d2e2d4f5e0
Nicolás Quezada Baier <david.avila@uach.cl>
2022-02-28 13:51:28 -0300

    se elimina target

--- Software/FullAxis/target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.PUBLIC_SETTINGS = {'app_name': 'FullAxis', 'author': 'Nicolas Quezada', 'version': '22.2.25', 'environment': 'local'}
