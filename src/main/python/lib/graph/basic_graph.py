a718321ec9ad65f1f637ed03c05485af20273609
Nicolás Quezada Baier <david.avila@uach.cl>
2023-06-21 21:12:24 -0400

    cambio de versión

--- src/main/python/lib/graph/basic_graph.py
+++
@@ -1,8 +0,0 @@
-from lib.basic_graph_ui import Ui_graph
-from PySide6.QtWidgets import QWidget
-
-
-class Widget_basic(QWidget, Ui_graph):
-    def __init__(self) -> None:
-        super().__init__()
-        self.setupUi(self)
--- src/main/python/lib/video/test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-vid = cv2.VideoCapture(0)
-
-
-serialNum = vid.get(cv2.CAP_PROP_MONOCHROME)
-print(serialNum)
