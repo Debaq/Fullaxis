ec2aba74a9b6129407433f864261b05e8d56110e
Nicolás Quezada Baier <david.avila@uach.cl>
2023-06-25 22:11:57 -0400

    Update ListCameras.py

--- src/main/python/lib/video/ListCameras.py
+++ src/main/python/lib/video/ListCameras.py
@@ -1,40 +1,22 @@
-import platform
+import usb.core
+import usb.util
 
-def list_video_devices_linux():
-    import pyudev
+# Encuentra los dispositivos USB
+devs = usb.core.find(find_all=True)
 
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by(subsystem='video4linux')
-
-    for device in context.list_devices(subsystem='video4linux'):
-        print(f"Device '{device.device_node}' hardware ID: {device.get('ID_SERIAL')}")
-
-def list_video_devices_windows():
-    import win32com.client
-
-    wmi = win32com.client.GetObject ("winmgmts:")
-    for usb in wmi.InstancesOf ("Win32_USBControllerDevice"):
-        dev = usb.Dependent
-        if 'VID_' in dev:
-            print(dev)
-
-def list_video_devices_mac():
-    # En macOS, no hay una forma directa de listar los dispositivos de video
-    # como en Linux o Windows. Sin embargo, puedes usar el comando 'system_profiler'
-    # para obtener información sobre los dispositivos USB, que pueden incluir
-    # dispositivos de video. Este es un ejemplo de cómo podrías hacerlo:
-    import subprocess
-
-    output = subprocess.check_output('system_profiler SPUSBDataType', shell=True)
-    print(output.decode())
-
-# Detecta el sistema operativo actual y llama a la función apropiada
-if platform.system() == 'Linux':
-    list_video_devices_linux()
-elif platform.system() == 'Windows':
-    list_video_devices_windows()
-elif platform.system() == 'Darwin':  # macOS
-    list_video_devices_mac()
-else:
-    print('Sistema operativo no soportado')
+# Itera sobre los dispositivos
+for dev in devs:
+    print('Dispositivo encontrado')
+    print('ID del producto: ', dev.idProduct)
+    print('ID del vendedor: ', dev.idVendor)
+    print('Número de serie: ', dev.serial_number)
+    print('Clase del dispositivo: ', dev.bDeviceClass)
+    print('Subclase del dispositivo: ', dev.bDeviceSubClass)
+    print('Protocolo del dispositivo: ', dev.bDeviceProtocol)
+    print('Número de configuraciones: ', dev.bNumConfigurations)
+    print('Fabricante: ', usb.util.get_string(dev, dev.iManufacturer))
+    print('Producto: ', usb.util.get_string(dev, dev.iProduct))
+    print('Número de serie: ', usb.util.get_string(dev, dev.iSerialNumber))
+    print('ID del vendedor:', hex(dev.idVendor))
+    print('ID del producto:', hex(dev.idProduct))
+    print('-----------------------------')
